e831706cc2f9e7f3cb97edb82dd3822422ac97e8
Rituviks <42962288+Rituviks@users.noreply.github.com>
2020-12-22 17:31:04 +0530

    Add files via upload

---
+++ String_Challange2.py
@@ -0,0 +1,3 @@
+number = "9,456:567&634;543%612!"
+print(number)
+print(number[1::4])
---
+++ String_challange1.py
@@ -0,0 +1,17 @@
+Parrot  = "Norwegian blue"
+print(Parrot)
+print(Parrot[3])
+print(Parrot[4])
+print()
+print(Parrot[3])
+print(Parrot[6])
+print(Parrot[-6])
+print("-"*80)
+print(Parrot[3:5])
+print(Parrot[3:9:2])
+print("-"*80)
+print(Parrot[:6] + Parrot[6:])
+print("-"*80)
+print(Parrot[0:6:2]) # Using Step
+print(Parrot[0:6:3]) #Using Step
+
