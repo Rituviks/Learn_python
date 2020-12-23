31915c59e47d966cd163781a3aac843859c0f36d
Rituviks <42962288+Rituviks@users.noreply.github.com>
2020-12-24 01:19:22 +0530

    Add files via upload

---
+++ Holiday_challange.py
@@ -0,0 +1,12 @@
+name = input("what is your name ")
+age = int(input("what is your age "))
+MAr_status = bool(input())
+print()
+if 18 <= age <= 45 and MAr_status == True:
+    print("you are welcome")
+
+else:
+    print("not suitable")
+
+
+
