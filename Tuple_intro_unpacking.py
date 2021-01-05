b98af19c5d00d2e1121a78f2820f29a8296485d6
Rituviks <42962288+Rituviks@users.noreply.github.com>
2021-01-05 11:41:26 +0530

    Add files via upload
    You Should get an error for the last line, As Tuple is Immutable... in a contrast to LIST..

---
+++ Tuple_intro_unpacking.py
@@ -0,0 +1,13 @@
+welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
+bad = "Bad Company", "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Emilda May", 2011
+metallica = "Ride the Lightning", "Metallica", 1984
+
+# unpacking
+album,band,Year = metallica
+print(album)
+print(band)
+print(Year)
+print(metallica)
+metallica.append("Yes")
